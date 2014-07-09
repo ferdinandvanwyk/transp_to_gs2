2c5a89572d3b5087ee6878762f3bf7afb7b477be
ferdinandvwyk <ferdinandvwyk@gmail.com>
2014-07-09 18:38:57 +0100

    ---Fixed reading from NetCDF

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 import scipy.interpolate as interp
-from scipy.io import netcdf
+from netCDF4 import Dataset
 
 in_file =  str(sys.argv[1])
-ncfile = netcdf.netcdf_file(in_file, 'r')
+ncfile = Dataset(in_file, 'r', format='NETCDF3')
 
 #Read relevant data from TRANSP file
 time = ncfile.variables['TIME']
+print time.shape
