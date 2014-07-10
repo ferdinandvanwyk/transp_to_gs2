b1fb93ff6c62dbd40d2e78527b4041366108a447
ferdinandvwyk <ferdinandvwyk@gmail.com>
2014-07-10 14:36:08 +0100

    ---Started gradients and sorted out printing.

--- main.py
+++ main.py
@@ -38,7 +38,10 @@ zeffp = ncfile.variables['ZEFFP'][t_idx,:]
 
 #Normalization quantities
 amin = 0.58044
-vth = 0  
+boltz_jk = 1.3806488e-23
+boltz_evk = 8.6173324e-5
+proton_mass = 1.672621777e-27
+
 
 ###################################
 #Calculate equilibrium parameters #
@@ -47,15 +50,32 @@ equil = {}
 equil['dens_1'] = np.interp(output_radius, x, ni)*1e6/1e19
 equil['dens_2'] = np.interp(output_radius, x, ne)*1e6/1e19
 equil['temp_1'] = np.interp(output_radius, x, ti)/1000 #keV
+vth = np.sqrt((2*equil['temp_1']*1000*boltz_jk/boltz_evk)/(2*proton_mass))
 equil['temp_2'] = np.interp(output_radius, x, te)/1000 #keV
 equil['omega'] = np.interp(output_radius, x, omega) #rad/s
 mag_axis_idx, min_value = min(enumerate(abs(bpbt)), key=operator.itemgetter(1))
 equil['btref'] = fbtx[mag_axis_idx]*btx[mag_axis_idx]
 equil['beta'] = 403.0*equil['dens_1']*equil['temp_1']/(1e5*equil['btref']**2)
 equil['zeff'] = np.interp(output_radius, x, zeffp)
+equil['qinp'] = np.interp(output_radius, x, q)
 
 # Gradient calculation requires numerical differentiation
 # Use a second order central method: f'(x) = [f(x+h) - f(x-h)]/2h
+# Find points closest points either side of output_radius and use those
+rad_idx, min_value = min(enumerate(abs(x - output_radius)), key=operator.itemgetter(1))
+if x[rad_idx] < output_radius:
+  rad_idx += 1
+else:
+  rad_idx -= 1
+
+equil['fprim_1'] = -(ni[rad_idx+1]-ni[rad_idx-1])/(x[rad_idx+1]-x[rad_idx-1])/ni[rad_idx]
+equil['fprim_2'] = -(ne[rad_idx+1]-ne[rad_idx-1])/(x[rad_idx+1]-x[rad_idx-1])/ne[rad_idx]
+equil['tprim_1'] = -(ti[rad_idx+1]-ti[rad_idx-1])/(x[rad_idx+1]-x[rad_idx-1])/ti[rad_idx]
+equil['tprim_2'] = -(te[rad_idx+1]-te[rad_idx-1])/(x[rad_idx+1]-x[rad_idx-1])/te[rad_idx]
+equil['g_exb'] = (omega[rad_idx+1]-omega[rad_idx-1])/(x[rad_idx+1]-x[rad_idx-1])*(x[rad_idx]/q[rad_idx])*(amin/vth)
+print equil['g_exb']
+print rad_idx, x[rad_idx+1]-x[rad_idx-1], omega[rad_idx+1]-omega[rad_idx-1], output_radius/equil['qinp'], amin/vth
+
 
 
 
