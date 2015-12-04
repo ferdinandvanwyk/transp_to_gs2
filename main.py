47f82e98a0914cfb8b443c99f4c6f67506d03554
Ferdinand van Wyk <ferdinandvwyk@gmail.com>
2015-12-04 14:12:39 +0000

    MAJOR: write fortran nml, add template feature

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, AutoMinorLocator
 from netCDF4 import Dataset
+import f90nml as nml
 
 
 def debye(temp, dens):
@@ -87,7 +88,13 @@ def Z(akappa, rhoc, th):
 
 in_file = str(sys.argv[1])
 output_radius = float(sys.argv[2])
+if output_radius > 1 or output_radius < 0:
+    raise ValueError('Please pick and output radius in the range [0,1].')
 output_time = float(sys.argv[3])
+try:
+    template = str(sys.argv[4])
+except:
+    template = None
 ncfile = Dataset(in_file, 'r', format='NETCDF3')
 
 # Create directory for plot checks and clear if already exists
@@ -95,10 +102,14 @@ plot_dir = 'plot_checks_rho_' + str(output_radius) + '_time_' + str(output_time)
 os.system('mkdir ' + plot_dir)
 os.system('rm -f ' + plot_dir + '/*')
 
-# Read relevant data from TRANSP file
+####################
+# Read TRANSP file #
+####################
 time = ncfile.variables['TIME'][:]
 
 # Convert input time to an index
+if output_time < np.min(time) or output_time >  np.max(time):
+    raise ValueError('Please pick a output time in the range [{:.3f},{:.3f}]'.format(np.min(time), np.max(time)))
 t_idx, min_value = min(enumerate(abs(time - output_time)),
                        key=operator.itemgetter(1))
 
@@ -141,6 +152,7 @@ psi_p = ncfile.variables['PLFLX'][t_idx, :]
 boltz_jk = 1.3806488e-23
 boltz_evk = 8.6173324e-5
 proton_mass = 1.672621777e-27
+e = 1.60217657e-19
 
 # Need to calculate factor which relates gradients in TRANSP psi_n
 # (=sqrt(psi_t/psi_tLCFS)) and Miller a_n (= diameter/diameter LCFS)
@@ -168,48 +180,59 @@ rad_idx, min_value = min(enumerate(abs(x - output_radius)),
 radb_idx, min_value = min(enumerate(abs(xb - output_radius)),
                           key=operator.itemgetter(1))
 
+##################################
+# Create new namelist dictionary #
+##################################
+
+if template:
+    gs2 = nml.read(template)
+else:
+    gs2 = {}
+ 
+gs2_keys = ['dist_fn_knobs', 'parameters', 'theta_grid_parameters',
+            'theta_grid_eik_knobs', 'species_parameters_1',
+            'species_parameters_2', 'species_parameters_3',
+            'species_parameters_4', 'species_parameters_5', 
+            'miscellaneous']
+
+for k in gs2_keys:
+    if k not in gs2.keys():
+        gs2[k] = {}
+
 ####################################
 # Calculate equilibrium parameters #
 ####################################
-equil = {}
-equil['amin'] = (rmaj[-1] - rmaj[0])/2/100
-amin = equil['amin']
+gs2['miscellaneous']['amin'] = (rmaj[-1] - rmaj[0])/2/100
+amin = gs2['miscellaneous']['amin']
 vth = np.sqrt((2*np.interp(output_radius, x, ti)*boltz_jk/boltz_evk)/ \
               (2*proton_mass))  # T(eV)
-equil['omega'] = np.interp(output_radius, x, omega)  # rad/s
+gs2['miscellaneous']['omega'] = np.interp(output_radius, x, omega)  # rad/s
 btor = fbtx*btx
 b = fbx*btx
-equil['bref'] = btor[mag_axis_idx]
+gs2['miscellaneous']['bref'] = btor[mag_axis_idx]
 # n_ref(1e19 m^-3), T_ref(keV):
-beta = 403.0*nd*1e6/1e19*ti/1000/(1e5*equil['bref']**2)
+beta = 403.0*nd*1e6/1e19*ti/1000/(1e5*gs2['miscellaneous']['bref']**2)
 if h_spec_bool:
     beta_full = 403.0*(nd*ti + nh*ti + nimp*timp + nb*tb + ne*te)* \
-                    1e6/1e19/1000/(1e5*equil['bref']**2)
+                    1e6/1e19/1000/(1e5*gs2['miscellaneous']['bref']**2)
 else:
     beta_full = 403.0*(nd*ti + nimp*timp + nb*tb + ne*te)*1e6/1e19/1000/ \
-                    (1e5*equil['bref']**2)
-equil['beta'] = np.interp(output_radius, x, beta)
-equil['zeff'] = np.interp(output_radius, x, zeffp)
+                    (1e5*gs2['miscellaneous']['bref']**2)
+gs2['parameters']['beta'] = np.interp(output_radius, x, beta)
+gs2['parameters']['zeff'] = np.interp(output_radius, x, zeffp)
 # See wiki definition: not taking into account B_T variation
-equil['beta_prime_input'] = (beta_full[rad_idx+1]-beta_full[rad_idx-1])/ \
+gs2['theta_grid_eik_knobs']['beta_prime_input'] = (beta_full[rad_idx+1]-beta_full[rad_idx-1])/ \
                             (x[rad_idx+1]-x[rad_idx-1])*dpsi_da
-equil['g_exb'] = (omega[rad_idx+1]-omega[rad_idx-1])/ \
+gs2['dist_fn_knobs']['g_exb'] = (omega[rad_idx+1]-omega[rad_idx-1])/ \
                     (x[rad_idx+1]-x[rad_idx-1])*(rho_miller/q[radb_idx])* \
                     (amin/vth)*dpsi_da  # q defined on xb grid
-equil['dpsi_da'] = dpsi_da
-equil['psi_tor'] = rho_transp
-equil['psi_pol'] = np.sqrt((psi_p[flux_idx - mag_axis_idx] - psi_p[0])/(psi_p[-1] - psi_p[0]))
-equil['bpol_flux_tube'] = np.interp(output_radius, x, bpol)
-equil['btor_flux_tube'] = btor[flux_idx]
-equil['mach'] = amin * equil['omega'] / vth
-
-outfile_name = 'gs2_rho_' + str(output_radius) + '_time_' + str(output_time) + '.in' 
-f = open(outfile_name, 'w')
-f.write('Equilibrium Parameters: \n')
-f.write('----------------------- \n')
-for name, value in sorted(equil.items()):
-    f.write(name + ' = ' + str(value) + '\n')
-f.write('\n')
+gs2['miscellaneous']['dpsi_da'] = dpsi_da
+gs2['miscellaneous']['psi_tor'] = rho_transp
+gs2['miscellaneous']['psi_pol'] = np.sqrt((psi_p[flux_idx - mag_axis_idx] - 
+                                      psi_p[0])/(psi_p[-1] - psi_p[0]))
+gs2['miscellaneous']['bpol_flux_tube'] = np.interp(output_radius, x, bpol)
+gs2['miscellaneous']['btor_flux_tube'] = btor[flux_idx]
+gs2['miscellaneous']['mach'] = amin * gs2['miscellaneous']['omega'] / vth
 
 ################################
 # Calculate species parameters #
@@ -218,31 +241,35 @@ f.write('\n')
 ############
 # Main ION #
 ############
-# ION SPECIES 1 - Deuterium (reference)
-ion_1 = {}
-ion_1['dens_1'] = 1.0
+gs2['species_parameters_1']['dens'] = 1.0
 n_ref = np.interp(output_radius, x, nd)*1e6/1e19  # 1e19m^-3
-ion_1['mass_1'] = 1.0
-ion_1['temp_1'] = 1.0
+gs2['species_parameters_1']['mass'] = 1.0
+gs2['species_parameters_1']['temp'] = 1.0
 t_ref = np.interp(output_radius, x, ti)/1000  # keV
-ion_1['fprim_1'] = -(nd[rad_idx+1]-nd[rad_idx-1])/(x[rad_idx+1]-x[rad_idx-1])/ \
-                     nd[rad_idx]*dpsi_da
-ion_1['tprim_1'] = -(ti[rad_idx+1]-ti[rad_idx-1])/(x[rad_idx+1]-x[rad_idx-1])/ \
-                     ti[rad_idx]*dpsi_da
+gs2['species_parameters_1']['fprim'] = -(nd[rad_idx+1]-nd[rad_idx-1])/ \
+                                         (x[rad_idx+1]-x[rad_idx-1])/ \
+                                         nd[rad_idx]*dpsi_da
+gs2['species_parameters_1']['tprim'] = -(ti[rad_idx+1]-ti[rad_idx-1])/ \
+                                         (x[rad_idx+1]-x[rad_idx-1])/ \
+                                         ti[rad_idx]*dpsi_da
+gs2['species_parameters_1']['type'] = 'ion'
+gs2['species_parameters_1']['z'] = 1
 
 ############
 # ELECTRON #
 ############
 electron = {}
-electron['dens_2'] = np.interp(output_radius, x, ne)*1e6/1e19/n_ref  # 1e19m^-3
+gs2['species_parameters_2']['dens'] = np.interp(output_radius, x, ne)*1e6/1e19/n_ref  # 1e19m^-3
 electron_dens = np.interp(output_radius, x, ne)*1e6/1e19  # 1e19m^-3
-electron['mass_2'] = 1.0/(2.0*1836.0)  # Assume D-electron plasma
-electron['temp_2'] = np.interp(output_radius, x, te)/1000/t_ref  # keV
+gs2['species_parameters_2']['mass'] = 1.0/(2.0*1836.0)  # Assume D-gs2['species_parameters_2'] plasma
+gs2['species_parameters_2']['temp'] = np.interp(output_radius, x, te)/1000/t_ref  # keV
 electron_temp = np.interp(output_radius, x, te)/1000  # keV
-electron['fprim_2'] = -(ne[rad_idx+1]-ne[rad_idx-1])/ \
+gs2['species_parameters_2']['fprim'] = -(ne[rad_idx+1]-ne[rad_idx-1])/ \
                         (x[rad_idx+1]-x[rad_idx-1])/ne[rad_idx]*dpsi_da
-electron['tprim_2'] = -(te[rad_idx+1]-te[rad_idx-1])/ \
+gs2['species_parameters_2']['tprim'] = -(te[rad_idx+1]-te[rad_idx-1])/ \
                         (x[rad_idx+1]-x[rad_idx-1])/te[rad_idx]*dpsi_da
+gs2['species_parameters_2']['type'] = 'electron'
+gs2['species_parameters_2']['z'] = -1
 
 ###############
 # Collisions  #
@@ -252,135 +279,113 @@ electron['tprim_2'] = -(te[rad_idx+1]-te[rad_idx-1])/ \
 loglam = 24.0 - np.log(1e4*np.sqrt(0.1*n_ref)/electron_temp)
 mi = 2
 zi = 1
-electron['vnewk_2'] = 3.95e-3*amin*np.sqrt(0.5*mi)*loglam*n_ref/ \
+gs2['species_parameters_2']['vnewk'] = 3.95e-3*amin*np.sqrt(0.5*mi)*loglam*n_ref/ \
                         (np.sqrt(t_ref)*electron_temp**1.5)
-ion_1['vnewk_1'] = 9.21e-5*amin*zi**4/np.sqrt(2.)*loglam*n_ref/t_ref**2
+gs2['species_parameters_1']['vnewk'] = 9.21e-5*amin*zi**4/np.sqrt(2.)*loglam*n_ref/t_ref**2
 
 #####################
 # Other ION species #
 #####################
 if h_spec_bool:
     # ION SPECIES 2 - Hydrogen
-    ion_2 = {}
-    ion_2['dens_3'] = np.interp(output_radius, x, nh)*1e6/1e19/n_ref  # 1e19m^-3
+    gs2['species_parameters_3']['dens_3'] = np.interp(output_radius, x, nh)*1e6/1e19/n_ref  # 1e19m^-3
     ion_2_dens = np.interp(output_radius, x, nh)*1e6/1e19  # 1e19m^-3
-    ion_2['mass_3'] = 0.5
-    ion_2['temp_3'] = np.interp(output_radius, x, ti)/1000/t_ref  # keV
+    gs2['species_parameters_3']['mass_3'] = 0.5
+    gs2['species_parameters_3']['temp_3'] = np.interp(output_radius, x, ti)/1000/t_ref  # keV
     ion_2_temp = np.interp(output_radius, x, ti)/1000  # keV
-    ion_2['fprim_3'] = -(nh[rad_idx+1]-nh[rad_idx-1])/ \
+    gs2['species_parameters_3']['fprim_3'] = -(nh[rad_idx+1]-nh[rad_idx-1])/ \
                       (x[rad_idx+1]-x[rad_idx-1])/nh[rad_idx]*dpsi_da
-    ion_2['tprim_3'] = -(ti[rad_idx+1]-ti[rad_idx-1])/ \
+    gs2['species_parameters_3']['tprim_3'] = -(ti[rad_idx+1]-ti[rad_idx-1])/ \
                       (x[rad_idx+1]-x[rad_idx-1])/ti[rad_idx]*dpsi_da
     zi = 1
-    ion_2['vnewk_3'] = 9.21e-5*amin*zi**4/np.sqrt(2.)*loglam*ion_2_dens/ \
+    gs2['species_parameters_3']['z'] = 1
+    gs2['species_parameters_3']['vnewk_3'] = 9.21e-5*amin*zi**4/np.sqrt(2.)*loglam*ion_2_dens/ \
                      ion_2_temp**2
+    gs2['species_parameters_3']['type'] = 'ion'
 
 # ION SPECIES 3 - Impurity
-ion_3 = {}
-ion_3['dens_4'] = np.interp(output_radius, x, nimp)*1e6/1e19/n_ref  # 1e19m^-3
+gs2['species_parameters_4']['dens_4'] = np.interp(output_radius, x, nimp)*1e6/1e19/n_ref  # 1e19m^-3
 ion_3_dens = np.interp(output_radius, x, nimp)*1e6/1e19  # 1e19m^-3
-ion_3['mass_4'] = 6.0
-ion_3['temp_4'] = np.interp(output_radius, x, timp)/1000/t_ref  # keV
+gs2['species_parameters_4']['mass_4'] = 6.0
+gs2['species_parameters_4']['temp_4'] = np.interp(output_radius, x, timp)/1000/t_ref  # keV
 ion_3_temp = np.interp(output_radius, x, timp)/1000  # keV
-ion_3['fprim_4'] = -(nimp[rad_idx+1]-nimp[rad_idx-1])/ \
+gs2['species_parameters_4']['fprim_4'] = -(nimp[rad_idx+1]-nimp[rad_idx-1])/ \
                      (x[rad_idx+1]-x[rad_idx-1])/nimp[rad_idx]*dpsi_da
-ion_3['tprim_4'] = -(timp[rad_idx+1]-timp[rad_idx-1])/ \
+gs2['species_parameters_4']['tprim_4'] = -(timp[rad_idx+1]-timp[rad_idx-1])/ \
                      (x[rad_idx+1]-x[rad_idx-1])/timp[rad_idx]*dpsi_da
 zi = 6
-ion_3['vnewk_4'] = 9.21e-5*amin*zi**4/np.sqrt(2.)*loglam*ion_3_dens/ion_3_temp**2
+gs2['species_parameters_4']['z'] = 6
+gs2['species_parameters_4']['vnewk_4'] = 9.21e-5*amin*zi**4/np.sqrt(2.)*loglam*ion_3_dens/ion_3_temp**2
+gs2['species_parameters_4']['type'] = 'ion'
 
 # FAST ION SPECIES
-ion_4 = {}
-ion_4['dens_5'] = np.interp(output_radius, x, nb)*1e6/1e19/n_ref  # 1e19m^-3
+gs2['species_parameters_5']['dens_5'] = np.interp(output_radius, x, nb)*1e6/1e19/n_ref  # 1e19m^-3
 ion_4_dens = np.interp(output_radius, x, nb)*1e6/1e19  # 1e19m^-3
-ion_4['mass_5'] = 1.0
-ion_4['temp_5'] = np.interp(output_radius, x, tb)/1000/t_ref  # keV
+gs2['species_parameters_5']['mass_5'] = 1.0
+gs2['species_parameters_5']['temp_5'] = np.interp(output_radius, x, tb)/1000/t_ref  # keV
 ion_4_temp = np.interp(output_radius, x, tb)/1000  # keV
-ion_4['fprim_5'] = -(nb[rad_idx+1]-nb[rad_idx-1])/ \
+gs2['species_parameters_5']['fprim_5'] = -(nb[rad_idx+1]-nb[rad_idx-1])/ \
                      (x[rad_idx+1]-x[rad_idx-1])/nb[rad_idx]*dpsi_da
-ion_4['tprim_5'] = -(tb[rad_idx+1]-tb[rad_idx-1])/ \
+gs2['species_parameters_5']['tprim_5'] = -(tb[rad_idx+1]-tb[rad_idx-1])/ \
                      (x[rad_idx+1]-x[rad_idx-1])/tb[rad_idx]*dpsi_da
 zi = 1
-ion_4['vnewk_5'] = 9.21e-5*amin*zi**4/np.sqrt(2.)*loglam*ion_4_dens/ion_4_temp**2
-
-f.write('Species Parameters: \n')
-f.write('------------------- \n')
-f.write('Deuterium (Reference): \n')
-for name, value in sorted(ion_1.items()):
-    f.write(name + ' = ' + str(value) + '\n')
-f.write('\n')
-f.write('Electron: \n')
-for name, value in sorted(electron.items()):
-    f.write(name + ' = ' + str(value) + '\n')
-f.write('\n')
-if h_spec_bool:
-    f.write('Hydrogen: \n')
-    for name, value in sorted(ion_2.items()):
-        f.write(name + ' = ' + str(value) + '\n')
-    f.write('\n')
-f.write('Impurity: \n')
-for name, value in sorted(ion_3.items()):
-    f.write(name + ' = ' + str(value) + '\n')
-f.write('\n')
-f.write('Beam Ions: \n')
-for name, value in sorted(ion_4.items()):
-    f.write(name + ' = ' + str(value) + '\n')
-f.write('\n')
+gs2['species_parameters_5']['z'] = 1
+gs2['species_parameters_5']['vnewk_5'] = 9.21e-5*amin*zi**4/np.sqrt(2.)*loglam*ion_4_dens/ion_4_temp**2
+gs2['species_parameters_5']['type'] = 'beam'
 
 ##################################
 # Calculate geoemetry parameters #
 ##################################
-geo = {}
-geo['rhoc'] = rho_miller
-geo['qinp'] = np.interp(output_radius, xb, q)
-geo['shat'] =  ((q[radb_idx+1]-q[radb_idx-1])/(xb[radb_idx+1]-xb[radb_idx-1]))* \
+gs2['theta_grid_parameters']['rhoc'] = rho_miller
+gs2['theta_grid_parameters']['qinp'] = np.interp(output_radius, xb, q)
+gs2['theta_grid_parameters']['shat'] =  ((q[radb_idx+1]-q[radb_idx-1])/(xb[radb_idx+1]-xb[radb_idx-1]))* \
                    (rho_miller/q[radb_idx])*dpsi_da
-geo['s_hat_input'] = ((q[radb_idx+1] - q[radb_idx-1])/ \
+gs2['theta_grid_parameters']['s_hat_input'] = ((q[radb_idx+1] - q[radb_idx-1])/ \
                       (xb[radb_idx+1] - xb[radb_idx-1]))* \
                      (rho_miller/q[radb_idx])*dpsi_da
-geo['shift'] = (flux_centres[rad_idx+1]/100/amin-flux_centres[rad_idx-1]/100/amin)/ \
+gs2['theta_grid_parameters']['shift'] = (flux_centres[rad_idx+1]/100/amin-flux_centres[rad_idx-1]/100/amin)/ \
                    (x[rad_idx+1]-x[rad_idx-1])*dpsi_da
-geo['akappa'] = np.interp(output_radius, x, elongation)
-geo['akappri'] = (elongation[rad_idx+1]-elongation[rad_idx-1])/ \
+gs2['theta_grid_parameters']['akappa'] = np.interp(output_radius, x, elongation)
+gs2['theta_grid_parameters']['akappri'] = (elongation[rad_idx+1]-elongation[rad_idx-1])/ \
                      (x[rad_idx+1]-x[rad_idx-1])*dpsi_da
-geo['tri'] = np.arcsin(np.interp(output_radius, x, triang))
-geo['tripri'] = (np.arcsin(triang[rad_idx+1])-np.arcsin(triang[rad_idx-1]))/ \
+gs2['theta_grid_parameters']['tri'] = np.arcsin(np.interp(output_radius, x, triang))
+gs2['theta_grid_parameters']['tripri'] = (np.arcsin(triang[rad_idx+1])-np.arcsin(triang[rad_idx-1]))/ \
                     (x[rad_idx+1]-x[rad_idx-1])*dpsi_da
-geo['rmaj'] = (rmaj[flux_idx] + rmaj[mag_axis_idx - (flux_idx-mag_axis_idx)])/100/2/amin
-geo['r_geo'] = (rmaj[-1] + rmaj[0])/2/100/amin
+gs2['theta_grid_parameters']['rmaj'] = (rmaj[flux_idx] + rmaj[mag_axis_idx - (flux_idx-mag_axis_idx)])/100/2/amin
+gs2['theta_grid_parameters']['r_geo'] = (rmaj[-1] + rmaj[0])/2/100/amin
 
-f.write('Geometry Parameters: \n')
-f.write('-------------------- \n')
-for name, value in sorted(geo.items()):
-    f.write(name + ' = ' + str(value) + '\n')
-f.write('\n')
-
-f.write('Miscellaneous Parameters (consistent with use of Miller parameters):\n')
-f.write('--------------------------------------------------------------------\n')
-f.write('irho = 2 \n')
-f.write('iflux = 0 \n')
-f.write('bishop = 4 \n')
-f.write('local_eq = .true. \n')
-f.write('\n')
+gs2['theta_grid_eik_knobs']['irho'] = 2
+gs2['theta_grid_eik_knobs']['iflux'] = 0
+gs2['theta_grid_eik_knobs']['bishop'] = 4
+gs2['theta_grid_eik_knobs']['local_eq'] = '.true.'
 
 #################################################
 # Calculate reference values for normalizations #
 #################################################
-e = 1.60217657e-19
-ref = {}
-ref['vref'] = vth
-ref['lref'] = amin
-ref['nref'] = n_ref*1e19
-ref['tref'] = t_ref*1000/boltz_evk
-ref['bref'] = equil['bref']
-larmor_freq_ref = e * ref['bref'] / (2*proton_mass)
-ref['rhoref'] = vth / larmor_freq_ref
 
-f.write('Reference Parameters: \n')
-f.write('--------------------- \n')
-for name, value in sorted(ref.items()):
-    f.write(name + ' = ' + str(value) + '\n')
-f.write('\n')
+gs2['miscellaneous']['vref'] = vth
+gs2['miscellaneous']['lref'] = amin
+gs2['miscellaneous']['nref'] = n_ref*1e19
+gs2['miscellaneous']['tref'] = t_ref*1000/boltz_evk
+larmor_freq_ref = e * gs2['miscellaneous']['bref'] / (2*proton_mass)
+gs2['miscellaneous']['rhoref'] = vth / larmor_freq_ref
+
+######################
+# Write the namelist #
+######################
+
+outfile_name = 'gs2_rho_' + str(output_radius) + '_time_' + str(output_time) + '.in' 
+for k1 in gs2.keys():
+    for k2 in gs2[k1].keys():
+        if type(gs2[k1][k2]) == np.float32 or type(gs2[k1][k2]) == np.float64:
+            gs2[k1][k2] = float(gs2[k1][k2])
+            
+gs2 = nml.Namelist(gs2)
+
+if outfile_name in os.listdir():
+    os.system('rm -rf ' + outfile_name)
+
+gs2.write(outfile_name)
 
 ####################
 # Diagnostic plots #
@@ -420,8 +425,8 @@ if h_spec_bool:
 theta = np.linspace(-np.pi, np.pi, 100)
 fig, ax = plt.subplots(1, 1)
 
-plt.plot(amin*R(geo['rmaj'], rho_miller, theta, geo['tri']),
-         amin*Z(geo['akappa'], rho_miller, theta))
+plt.plot(amin*R(gs2['theta_grid_parameters']['rmaj'], rho_miller, theta, gs2['theta_grid_parameters']['tri']),
+         amin*Z(gs2['theta_grid_parameters']['akappa'], rho_miller, theta))
 plt.xlim(0, 6*amin)
 plt.title(r'Flux Surface at $\rho_c = {:.3f}$'.format(rho_miller))
 ax.set_aspect('equal')
